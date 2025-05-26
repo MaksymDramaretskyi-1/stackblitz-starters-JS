fa4186d643b0053e4ea08f68d06a02c732d4b39f
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-05-26 14:55:56 +0200

    JS-Function-Form

--- script.js
+++ script.js
@@ -1,31 +1,83 @@
 'use strict';
-const userAge = 18;
+// const userAge = 18;
 
 /*Sjekker om userAge er mindre enn 18; og gir respons ut i fra taltet*/
-if (userAge > 18) {
-  console.log('Du kan ikke komme inn');
-} else if (userAge === 18) {
-  console.log('Velkommen inn! Akkurat blitt 18 gratis bursdagsdrink ');
-} else if (userAge > 60 && userAge < 65) {
-  console.log('Senior rabatt på drinker!');
-} else if (userAge >= 18) {
-  console.log('Velkommen inn');
-} else {
-  console.log('Noe er galt med koden');
-}
+// if (userAge > 18) {
+//   console.log('Du kan ikke komme inn');
+// } else if (userAge === 18) {
+//   console.log('Velkommen inn! Akkurat blitt 18 gratis bursdagsdrink ');
+// } else if (userAge > 60 && userAge < 65) {
+//   console.log('Senior rabatt på drinker!');
+// } else if (userAge >= 18) {
+//   console.log('Velkommen inn');
+// } else {
+//   console.log('Noe er galt med koden');
+// }
 
 /*FØRERKORT*/
 /*Sjekker om userAge er større enn eller samme som 18, og gir respons ut i fra tallet */
-if (userAge >= 18) {
-  console.log('Du kan ta førerkortet');
-} else {
-  console.log('Du kan ikke ta førerkortet');
+// if (userAge >= 18) {
+//   console.log('Du kan ta førerkortet');
+// } else {
+//   console.log('Du kan ikke ta førerkortet');
+// }
+
+// const userName =  "Maksym";
+// const userPassword = "Maskym123";
+// let userOnline = false;
+// let page = 'Log-in';
+
+// if (userName == " " && userPassword == ""){
+//     console.log('Du har ikke lagt brukernavn og passord')
+// } else if(userName != "Maksym"){
+//     console.log(`Du gar suksesfull logget inn ${userName}`)
+// } else if(userName != "Maksym") {
+//   console.log("Brukernavnet er feil. Prøv på nytt.")
+// } else if (userPassword != "Maskym123"){
+//   console.log("Passsword er feil. Prøv på nytt.")
+// } else if (userName != "Maksym" || userPassword != "Maskym123"){
+//   console.log("Brukernavnet eller passordet er feil. Prøv på nytt")
+// } else if (userName == "Maksym" && userPassword == "Maskym123"){
+//   console.log("Brukernavnet OG passordet er feil. Prøv på nytt")
+//   userOnline = true;
+//   page = "Home";
+//   const loginMessage = document.getElementById('loginMessage');
+//   console.log(loginMessage);
+//   loginMessage.textContent = `Du gar suksesfull logget inn ${userName}`;
+// }else {
+//   console.log("Noe er galt med koden...")
+// }
+
+let inputValueUser = document.getElementById("inputValue");
+let conversionSelect = document.getElementById("conversionType");
+let resultParagraph =  document.getElementById("result");
+let convertButton = document.getElementById("convertButton");
+
+function convert () {
+  const value = parseFloat(inputValueUser.value);
+  const type = conversionSelect.value;
+  let result;
+
+
+  if (isNaN(value)) {
+    resultParagraph.textContent = "Пожалуйста, введите корректное число.";
+    return;
+  }
+
+
+  if (type === "cmToM") {
+    result = value / 100;
+    resultParagraph.textContent = `${value} cm = ${result} m`;
+  } else if (type === "kgToG") {
+    result = value * 1000;
+    resultParagraph.textContent = `${value} kg = ${result} g`;
+  } else {
+    resultParagraph.textContent = "Неизвестный тип конвертации.";
+  }
+
+  event.preventDefault();
 }
 
-const userName =  '';
-let userOnline = false;
-let page = 'Home';
 
-if (userName == " " && userPassword == ""){
-  console.log('Du har ikke lagt brukernavn og passord')
-}
+
+convertButton.addEventListener("click",convert);
